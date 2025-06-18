c4b4b3294dee875fb57713ee28f1d7d16b4dab9a
Daniel Schefer <danielschefer2007@hotmail.com>
2025-06-18 10:14:19 -0300

    Initial Files

---
+++ Album.cs
@@ -0,0 +1,11 @@
+﻿class Album
+{
+    private List<Musica> musicaList = new List<Musica>();
+    public string Nome { get; set; }
+    public int DuracaoTotal { get; set; }
+
+    public void AdicionarMusica(Musica musica)
+    {
+        musicaList.Add(musica);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Album albumDoQueen = new Album();
+albumDoQueen.Nome = "A night ate the opera";
+
+Musica musica1 = new Musica();
+musica1.Nome = "Love of my life";
+
+Musica musica2 = new Musica();
+musica2.Nome = "Bohemian Rhapsody";
